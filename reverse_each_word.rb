85fb1c045e3efab24920b67b64bce6287bf7992f
Christina Benbeneck <cbenbeneck@mail.usf.edu>
2018-10-25 16:36:42 +0000

    Done.

---
+++ reverse_each_word.rb
@@ -0,0 +1,9 @@
+def reverse_each_word(sentence)
+  sentence = sentence.split.each { | n | n.reverse! } 
+  sentence.join(" ")
+end 
+
+def reverse_each_word(sentence)
+  sentence = sentence.split.collect { | n | n.reverse! } 
+  sentence.join(" ")
+end 
