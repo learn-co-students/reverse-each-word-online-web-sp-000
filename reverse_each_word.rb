6474e765d215c9bbfbc365f78e2440fdbe1b2ac4
Ezenwa Onukwue <ezenwa.onukwue@salesloft.com>
2018-11-15 02:45:11 +0000

    Done.

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,6 +1,8 @@
-def reverse_each_word(string)
-  string = "Hello there, and how are you?".split
-  string.each |word|
-  word.reverse
+def reverse_each_word(sentence1)
+  new_array = []
+  sentence = sentence1.split
+  sentence.collect do |word|
+  new_array << word.reverse
+  end
+  return new_array.join(" ")
 end
-return string
