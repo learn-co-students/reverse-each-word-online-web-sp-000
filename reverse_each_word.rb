df3173afcdb3cbe6c4209d59a1887c081e50c5e6
Alexander Baker <alex.baker685@gmail.com>
2018-11-27 23:59:33 +0000

    Done.

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,8 +1,8 @@
 require 'pry'
 def reverse_each_word(sentence)
-array = sentence.split
-
-reversedarray = array.collect  {|word| word.reverse }
-reversedsentence = reversedarray.join(" ")
-reversedsentence
+  #array = sentence.split
+  #reversedarray = array.collect  {|word| word.reverse }
+  #reversedsentence = reversedarray.join(" ")
+  #reversedsentence
+  sentence.split.collect {|word| word.reverse}.join(" ")
 end
