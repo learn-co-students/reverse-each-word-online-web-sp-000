0973477d2d5ce1e9145a9366ae656e72d6973faa
kathinka martinsen <kathinkamartinsen@gmail.com>
2018-12-18 19:52:02 +0000

    Done.

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,9 +1,22 @@
 
- def reverse_each_word(string)
-  array = string.split(" ") #turn string into an array
-  test_array = []
-  array.collect do|string| #iterate over the array
-    test_array << string.reverse #reverse each word in the array
-  end
-  test_array.join(" ")
+ #def reverse_each_word(string)
+#  array = string.split(" ") #turn string into an array
+#  test_array = []
+#  array.collect do|string| #iterate over the array
+#    test_array << string.reverse #reverse each word in the array
+#  end
+#  test_array.join(" ")
+#end
+
+
+#better refactor
+
+def reverse_each_word(string)
+  string.split.collect {|word| word.reverse}.join(" ")
 end
+
+# no iteration:
+
+# def reverse_each_word(string)
+#  string.reverse.split.reverse.join(" ")
+# end
