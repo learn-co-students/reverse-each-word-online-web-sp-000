0709847807bde0b4af06da86a2f9aa1e6be7c194
J <jennifer.singleton@gmail.com>
2019-01-13 19:54:15 +0000

    Done.

---
+++ reverse_each_word.rb
@@ -0,0 +1,8 @@
+def reverse_each_word(sentence)
+  sentence_array = sentence.split(" ")
+  #new_array = []
+  new_array = sentence_array.collect do |word|
+    word.reverse
+  end
+  new_array.join(" ")
+end
