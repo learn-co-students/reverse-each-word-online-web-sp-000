b6adac8391784bb000733dd03b5af2e21444c307
Kirill Shcherbina <rabbichaim770@gmail.com>
2019-03-26 19:27:49 +0000

    Done.

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,8 +1,15 @@
+#def reverse_each_word(sentence)
+#first_array = sentence.split(" ")
+#new_array = []
+#first_array.each do |sentence|
+#new_array << sentence.reverse
+#end
+#new_array.join(" ")
+#end
+
 def reverse_each_word(sentence)
-  sentence.split("")
-  new_array = []
-  sentence.each do |stuff|
-    new_array << sentence.reverse
-  end
-  new_array
+first_array = sentence.split(" ").collect do |sentence|
+sentence.reverse
+end
+first_array.join(" ")
 end
