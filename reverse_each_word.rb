337cf4f557c44987de52dcfb412114e97b8fe3a7
Heeraj Ramsanai <ramsanai_heeraj@live.com>
2019-05-17 19:01:31 +0000

    Done.

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,7 +1,7 @@
 
 def reverse_each_word(sentence)
   newwords = []
-  sentence.split.each do|words|
+  sentence.split.collect do|words|
     newwords<<words.reverse
   end
   return newwords.join(" ")
