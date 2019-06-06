76e40dcced2961dc14df960897acc6ff791362eb
Connor Cowan <ccowan003@comcast.net>
2019-06-06 00:59:00 +0000

    Done.

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,6 +1,12 @@
 def reverse_each_word(sentence)
-  reversed_array = []
-  sentence.collect { |phrase|
-    reversed_array << phrase
-  
+  sentence.split.collect {|word|
+    word.reverse
+  }
+  .join(" ")              #converts array to string
 end
+
+# Without iterating:
+
+# def reverse_each_word(sentence)
+#  sentence.reverse.split.reverse.join(" ")           #You cant use enumerators on a string
+# end
