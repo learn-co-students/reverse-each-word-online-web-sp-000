3933b6769c4287cedba85ccfcaeb8dd0b03e935f
Lindsey Wells <lindsey.wells84@gmail.com>
2019-06-21 10:34:27 +0000

    Done.

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -3,7 +3,7 @@
 def reverse_each_word(string)
   new_array = string.split(" ")
   reversed_array = []
-  new_array.collect do |word|
+  new_array.each do |word|
     reversed_array << word.reverse
 end
 reversed_array.join(" ")
