633e5704ac8ed2e0f2da41b49434d4bd8181e0a1
Blythe Pollard <blytheepollard@gmail.com>
2019-09-10 04:21:42 +0000

    Done.

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,13 +1,3 @@
-
-#def reverse_each_word(sentence)
- # new_array = [] #why do we need a new array?
-  #backwards = sentence.split
-#  backwards.each do |new_sentence|
- #   new_array << new_sentence.reverse
-#end  
-#new_array.join(" ")
-#end
-
 def reverse_each_word(sentence)
   new_array = []
   backwards = sentence.split
