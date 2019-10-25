e889bad88dc136133a8791f6b8f1bdac03da2a7f
Aoune Syed <aoune007@gmail.com>
2019-10-25 04:16:24 +0000

    Done.

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,6 +1,4 @@
-def reverse_each_word(sentence)
-  sentence.split do |word| word.reverse.join
-    "Hello there, and how are you?"
-  end
 
+def reverse_each_word(sentence)
+sentence.split.collect {|letters| letters.reverse}.join(" ")
 end
