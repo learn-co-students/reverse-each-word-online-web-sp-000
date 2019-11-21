443a1d1e9b39c7b8a6b71af090fda83daa80c522
Jason J Cruz <jaycru06@yahoo.com>
2019-11-21 04:56:55 +0000

    Done.

---
+++ reverse_each_word.rb
@@ -0,0 +1,11 @@
+ def reverse_each_word(sentence) 
+   reverse_array = [] 
+   sentence.split.each do |word| 
+     reverse_array << word.reverse 
+   end 
+   reverse_array.join(" ") 
+end 
+
+def reverse_each_word(sentence)
+   sentence.split.collect {|word| word.reverse}.join(" ")
+ end 
