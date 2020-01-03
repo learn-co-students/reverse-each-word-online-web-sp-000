df59ec7db33e3c9ddeddd515c5c4baacf5563604
Jonathan Koss <jonathan_koss@yahoo.com>
2020-01-03 01:55:24 +0000

    Done.

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,8 +1,6 @@
 require 'pry'
 
 def reverse_each_word(sentence)
-  s_to_a = sentence.split
-  rev_words = s_to_a.collect {|word| word.reverse} 
-  rev_words.join(" ")
+  sentence.split.collect {|word| word.reverse}.join(" ")
 end 
 
