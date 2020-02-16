5f3c0210ce9d6767e8866b514787512555ebdd00
Will Corcoran <corcoranwill@gmail.com>
2020-02-16 21:01:01 +0000

    Done.

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,11 +1,13 @@
-def reverse_each_word(words)
-  a = words.split
+def reverse_each_word(sentence)
+  sentence.split.collect {|word| word.reverse}.join(" ")
 
-  a_rev_each = []
-  a.each {|x| a_rev_each << x.reverse}
-  words_rev_each = a_rev_each.join(" ")
+  # a = sentence.split
 
-  a_rev_collect = []
-  a.collect {|x| a_rev_collect << x.reverse}
-  words_rev_collect = a_rev_collect.join(" ")
+  # a_rev_each = []
+  # a.each {|x| a_rev_each << x.reverse}
+  # words_rev_each = a_rev_each.join(" ")
+  #
+  # a_rev_collect = []
+  # a.collect {|x| a_rev_collect << x.reverse}
+  # words_rev_collect = a_rev_collect.join(" ")
 end
