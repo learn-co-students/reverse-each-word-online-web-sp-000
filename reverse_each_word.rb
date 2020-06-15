4dabb2ba805983cc989e799c1fdcbcf870412c16
reevesalic <reevesalic@gmail.com>
2020-06-14 22:22:48 -0400

    Done.

---
+++ reverse_each_word.rb
@@ -0,0 +1,9 @@
+
+def reverse_each_word(sentence)
+  sentence = []
+  sentence.collect do |word|
+  puts word.reverse
+     #puts sentence.reverse(" ")
+  end
+  #reverse_each_word(sentence_1(sentence))
+end 
