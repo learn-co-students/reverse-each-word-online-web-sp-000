6e4358fcb322a486774c8d7baab405efdffaa211
Leslie G <lesliegonzalez@me.com>
2020-07-03 22:30:08 +0000

    Done.

---
+++ reverse_each_word.rb
@@ -0,0 +1,9 @@
+sentence = "Hello there, and how are you?"
+
+def reverse_each_word(sentence)
+  sentence.reverse.split.reverse.join(" ")
+end
+
+def reverse_each_word(sentence)
+  sentence.split.collect {|word| word.reverse}.join(" ")
+end
