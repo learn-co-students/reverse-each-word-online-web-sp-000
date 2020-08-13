08ca740ddb3d7fcb1d43659f6066b1a2b65a1ca7
shacoca <shacoca@yahoo.com>
2020-08-13 15:22:32 -0700

    Done.

---
+++ reverse_each_word.rb
@@ -0,0 +1,10 @@
+def reverse_each_word(string)
+    # string argument of a sentence and returns that same sentence with each word reversed in place.
+    words = string.split(" ")
+    
+end
+
+=begin
+def reverse_each_word(string)
+end
+=end
