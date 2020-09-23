214cdf5d7a0e61055432d8fcff62d49800ac0fd1
Ryan K Burns <ryankburns@gmail.com>
2020-09-23 04:27:58 +0000

    Done.

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,5 +1,8 @@
-def reverse_each_word(arg)
-  arg.each do |backward|
-    backward.reverse
-  end
+def reverse_each_word(string)
+  backwards = []
+  array = string.split(" ")
+    array.collect do |words|
+    backwards << words.reverse
+end
+backwards.join(" ")
 end
