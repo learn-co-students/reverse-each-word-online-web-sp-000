73a2d04e4c355df0e2741c59a7f623405f1e417e
Ian King <kickthechair@gmail.com>
2021-05-29 13:48:54 +0000

    Done.

---
+++ reverse_each_word.rb
@@ -0,0 +1,14 @@
+#method to reverse each word in a string in place.
+def reverse_each_word(sentance)
+  string_to_array = sentance.split(" ")
+  reversed_array = []
+  string_to_array.each { |element| reversed_array << element.reverse }
+  reversed_array.join(" ")
+end
+
+
+#same method to reverse each word in a string in place - but using #collect/#map
+def reverse_each_word(sentance)
+  string_to_array = sentance.split(" ")
+  string_to_array.collect { |element| element.reverse }.join (" ")
+end
